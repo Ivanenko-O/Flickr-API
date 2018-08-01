abe0d93c51ffc6c295708a31e78f5f79f68875b6
olko <oles.ivanenko@gmail.com>
2018-08-01 14:36:57 +0300

    Added smothscroll, focus, loading; deleted jQ,

--- index.js
+++ index.js
@@ -1,62 +1,191 @@
-(function() {
-    // var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://api.flickr.com/services/rest/?method=flickr.favorites.getPublicList&api_key=25087e994d7f8eca9e31016fbcb58e89&user_id=155573498%40N02&extras=url_q&per_page=50&format=json&nojsoncallback=1&auth_token=72157696133274612-a6e48b53dbd4ec4b&api_sig=6e8cef140656cfd6d829a686352e870f', true);
-    xhr.send();
+const COLS = 5;
+const ROWS = parseInt(window.innerHeight / 150);
+const ROWS_BEFORE_LOADING = COLS * 3;
 
-    
-xhr.onreadystatechange = function() {
+const API_KEY = "b9b865a7a966d097e5e8073a81b39a83";
+const PER_PAGE = 50;
+const USER_ID = "155573498%40N02";
 
-    if (this.readyState != 4) return;
+const IMAGE_HEIGHT = 150; //px
 
-    if (xhr.status != 200) {
-        console.log( xhr.status + ': ' + xhr.statusText ); // 404: Not Found
-      } 
-        
-        var response = JSON.parse(xhr.responseText);
-        var photos = response.photos.photo;
-        
+var firstLoad = true;
+var curPage = 1;
+var arr = []; //global array with images
+// var lastIndex = 0;
+var isLoading = false;
 
-            for(let i = 0; i < photos.length; i++) {
-                let wrap_img = document.createElement("div");
-                wrap_img.className = "img-wrap";
-                var img = new Image();
-                img.src = photos[i].url_q;
-                img.alt = photos[i].title;
-                img.tabindex = i;
+document.onkeydown = keyboardHandler;
 
-                wrap_img.appendChild(img);
+getData(1);
 
-                document.getElementById('images').appendChild(wrap_img);
-            }
+function getData(page, callback) {
+	if (isLoading) { //do not load next page until current is completely loaded
+		return;
+	}
+	isLoading = true;
+	var xhr = new XMLHttpRequest();
 
+	xhr.open(
+		"GET",
+		"https://api.flickr.com/services/rest/?method=flickr.favorites.get" +
+		"PublicList&api_key=" +
+		API_KEY +
+		"&user_id=" +
+		USER_ID +
+		"&extras=url_q&per_page=" +
+		(firstLoad ? PER_PAGE : PER_PAGE - 10) +
+		"&format=json&nojsoncallback=1&page=" +
+		page,
+		true
+	);
+	xhr.send();
+
+	xhr.onreadystatechange = e => {
+		var xhr = e.target;
+		if (xhr.readyState != 4) return;
+
+		if (xhr.status != 200) {
+			console.log(xhr.status + ": " + xhr.statusText); // 404: Not Found
+		}
+
+		showData(xhr.responseText, page);
+		isLoading = false;
+	};
+}
+
+function showData(response, page) {
+	var response = JSON.parse(response);
+	var photos = response.photos.photo;
+	console.log('number of new photos', photos.length);
+
+  if (page === 2 ) {
+    photos.splice(0, 10);
+  }
+
+	for (let i = 0; i < photos.length; i++) {
+
+		var wrap_img = document.createElement("div");
+		wrap_img.className = "img-wrap";
+
+		var lastIndex = arr.length > 0 ? +arr[arr.length - 1].getAttribute("tabindex") : -1;
+		// var lastIndex = arr.length > 0 ? +arr[arr.length - 1].getAttribute("tabindex") : -1;
+		wrap_img.setAttribute("tabindex", lastIndex + 1);
+
+		var img = new Image();
+		img.src = photos[i].url_q;
+		img.alt = photos[i].title;
+
+		wrap_img.appendChild(img);
+		document.getElementById("images").appendChild(wrap_img);
+		arr.push(wrap_img);
+	}
+
+	updateFocus(page);
+}
+
+function updateFocus(page) {
+	console.log('number of all photos: ', arr.length);
+	console.log("page" + page);
+
+
+	var index = 0;
+	if (firstLoad) {
+		firstLoad = false;
+		index = 0;
+	} else {
+		var curIndex = document.activeElement.tabIndex;
+		index = Math.min(curIndex, arr.length - 1);
+  }
+  
+	arr[index].focus();
+}
+
+function keyboardHandler(e) {
+	if (isLoading) {
+		return false;
+	}
+	var curIndex = +document.activeElement.tabIndex;
+	var prev = curIndex;
+
+	switch (e.keyCode) {
+		case 37: // left
+			curIndex = Math.max(curIndex - 1, 0);
+			if (needLoadMore(curIndex)) {
+				loadMore();
+			}
+			break;
+		case 39: // right
+			curIndex = Math.min(curIndex + 1, arr.length - 1);
+			if (needLoadMore(curIndex)) {
+				loadMore();
+			}
+			break;
+		case 40: // doown
+
+			curIndex = Math.min(curIndex + COLS, arr.length - 1);
+			if (needLoadMore(curIndex)) {
+				var a = COLS * ROWS * curPage - COLS * 2;
+				loadMore();
+			}
+			scrollTo(document.documentElement, (Math.floor(curIndex / 5) - 1) * IMAGE_HEIGHT, 200);
+			break;
+		case 38: // up
+			curIndex = Math.max(curIndex - COLS, 0);
+			scrollTo(document.documentElement, (Math.floor(curIndex / 5) - 1) * IMAGE_HEIGHT, 200);
+			break;
+		case 9: //tab
+			return false;
+	}
+
+	arr[curIndex].focus();
+	return curIndex == prev;
+}
+
+function needLoadMore(curIndex) {
+	// var curPerPage = PER_PAGE;
+	var limit = arr.length - ROWS_BEFORE_LOADING;
+	// var limit = PER_PAGE + ((PER_PAGE - 10) * (curPage - 1)) - COLS * 2;
+	console.log("limit:" + limit);
+
+	return curIndex >= limit;
+}
+
+function loadMore() {
+	curPage++;
+	console.log("load page " + curPage);
+	getData(curPage);
 }
 
 
-    // var flickerAPI = "https://api.flickr.com/services/rest/?method=flickr.favorites.getPublicList&api_key=25087e994d7f8eca9e31016fbcb58e89&user_id=155573498%40N02&extras=url_q&per_page=50&format=json&nojsoncallback=1&auth_token=72157696133274612-a6e48b53dbd4ec4b&api_sig=6e8cef140656cfd6d829a686352e870f";
-    // $.getJSON( flickerAPI, {
-    // // tags: "mount rainier",
-    // // tagmode: "any",
-    // // format: "json"
-    // })
-    // .done(function( data ) {
-    //     $.each( data.photos.photo, function( i, item ) {
-        
-    //     $("<div class='img-wrap'></div>")
-    //         .appendTo( "#images" )
-    //         .append($( "<img>" )
-    //         .attr( "src", item.url_q )
-    //         .attr("alt", item.title));
+function scrollTo(element, to = 0, duration = 1000) {
 
-    //         console.log(i);
-            
-    //         if ( i === 50 ) {
-    //             return false;
-    //         }
+	const start = element.scrollTop;
+	const change = to - start;
+	// console.log(start);
 
-    //     });
-    //     console.log(data.photos.photo);
-        
-    // });
-})();
-   
+	const increment = 20;
+	let currentTime = 0;
+
+	const animateScroll = (() => {
+
+		currentTime += increment;
+
+		const val = Math.easeInOutQuad(currentTime, start, change, duration);
+
+		element.scrollTop = val;
+
+		if (currentTime < duration) {
+			setTimeout(animateScroll, increment);
+		}
+	});
+
+	animateScroll();
+};
+
+Math.easeInOutQuad = function (t, b, c, d) {
+
+	t /= d / 2;
+	if (t < 1) return c / 2 * t * t + b;
+	t--;
+	return -c / 2 * (t * (t - 2) - 1) + b;
+};
